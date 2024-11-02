ce089be8ffe96a0253dfb886de522fd47cb6d136
조다니엘(Daniel Cho) <arduinocc04@gmail.com>
2024-11-02 11:24:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ class Game:
         self.logfile.write(str(self.player.rel) + "\n")
         keys = pg.key.get_pressed()
         self.logfile.write(str(8*keys[pg.K_w] + 4*keys[pg.K_a] + 2*keys[pg.K_s] + keys[pg.K_d]) + "\n")
+        self.logfile.write(str(int(self.player.shot)) + "\n")
         self.object_handler.update()
         self.weapon.update()
         pg.display.flip()
